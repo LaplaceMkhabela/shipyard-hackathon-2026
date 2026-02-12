de8d6a2537732a1cc65e07919e1c6083c8add41d
samkhjhb025 <samkhjhb025@student.wethinkcode.co.za>
2026-02-12 20:58:49 +0200

    project setup

---
+++ cook-easy/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cook-easy',
+  webDir: 'dist'
+};
+
+export default config;
