dbb1a363b014bb4dc514e7b797331a155735f738
Sabelo Mkhabela <transformmkhabela@gmail.com>
2026-02-12 20:42:59 +0200

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'cook-easy-react',
-  webDir: 'dist'
-};
-
-export default config;
